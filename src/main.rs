b28170788f91740b60d42523f0a16c015046c815
Violet <violet.oh@hobbylobby.com>
2024-09-06 14:33:55 -0500

    Adding tuple

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,15 @@ fn main() {
 
 fn tuple() {
     // Group multiple items of mixed data types
+    // Elements are ordered
+    // Stored in a fixed-length, contiguous section memory
+    let mut stuff: (u8, f32, char) = (10, 3.14, 'x');
+    stuff.0 += 3;
+    let first_item = stuff.0;
+    println!("first_item is {}", first_item);
+
+    let (a, b, c) = stuff;
+    println!("b is {}", b);
 }
 
 fn multi_dimensional_array() {
